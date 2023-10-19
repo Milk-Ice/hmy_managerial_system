7530e5d1e64052bdf421b2f5baa608f0d65d2cea
Milk-Ice <mathila1218@gmail.com>
2023-10-19 09:47:29 +0800

    02_Element Plus集成_App宽高铺满

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <script setup lang="ts">
-
+import { Delete, Edit, Search, Share, Upload } from '@element-plus/icons-vue'
 </script>
 
 <template>
-  <div>App</div>
-  <router-link to="/login">login</router-link>
-  <router-link to="/main">main</router-link>
-  <router-view></router-view>
+  <div class="app">App
+    <el-button type="primary" :icon="Edit" />
+    <router-link to="/login">login</router-link>
+    <router-link to="/main">main</router-link>
+    <router-view></router-view>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.app {
+  width: 100vw;
+  height: 100vh;
+  /* background-color: green; */
+}
+</style>
