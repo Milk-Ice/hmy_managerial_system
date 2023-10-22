fd57660e742f710115cfd44e40b6f138f4eb09ad
Milk-Ice <mathila1218@gmail.com>
2023-10-22 23:43:31 +0800

    04_动态路由_基于角色添加路由_基于菜单动态匹配_动态创建页面和路由对象

---
+++ src/router/main/analysis/department/department.ts
@@ -0,0 +1,7 @@
+const department = () => import('@/views/main/analysis/department/department.vue')
+export default {
+  path: '/main/analysis/department',
+  name: 'department',
+  component: department,
+  children: []
+}
---
+++ src/router/main/analysis/menu/menu.ts
@@ -0,0 +1,7 @@
+const menu = () => import('@/views/main/analysis/mwnu/menu.vue')
+export default {
+  path: '/main/analysis/menu',
+  name: 'menu',
+  component: menu,
+  children: []
+}
