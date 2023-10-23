e72dc38a2bef133564bbea3a9a5c2ee4e03fca75
Milk-Ice <mathila1218@gmail.com>
2023-10-23 10:22:07 +0800

    04_动态路由_根据菜单动态的映射路由

--- src/store/login/login.ts
+++ src/store/login/login.ts
@@ -8,6 +8,7 @@ import type { IAccount } from '@/types/login'
 import { localCache } from '@/utils/cache'
 import router from '@/router'
 import { LOGIN_TOKEN } from '@/gobal/constants'
+import type { RouteRecordRaw } from 'vue-router'
 
 interface ILoginState {
   token: string
@@ -43,6 +44,28 @@ const useLoginStore = defineStore('login', {
       // 4.进行本地缓存
       localCache.setCache('userInfo', userInfo)
       localCache.setCache('userMenus', userMenus)
+
+      // 1.获取菜单(userMenus代码写在位置)
+
+      // 2.动态获取所有的路由对象
+      // *放到数组路由对象都在独立的文件中
+      // *从文件中将所有路由对象先读取数组
+      const localRoute: RouteRecordRaw[] = []
+      // 获取所有的ts文件
+      const files = import.meta.glob('../../router/main/**/*.ts', { eager: true })//拿到的是对象类型
+      // console.log(files)
+      for (const key in files) {
+        const module = files[key]
+        // console.log(module.default)
+        localRoute.push(module.default)
+      }
+      // 3.根据菜单去匹配正确的路由router.addRoute('main', xxx)
+      for (const menu of userMenus) {
+        for (const submenu of menu.children) {
+          const route = localRoute.find((item) => item.path === submenu.url)
+          if (route) router.addRoute('main', route)
+        }
+      }
 
       // 5.页面跳转(main页面)
       router.push('/main')
