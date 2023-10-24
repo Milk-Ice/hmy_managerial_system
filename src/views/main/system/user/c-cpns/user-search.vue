d34b8aaea814cf13ca42ec3599f05b8d4b4b2da2
Milk-Ice <mathila1218@gmail.com>
2023-10-24 19:49:53 +0800

    05_用户User界面_search搜索区域的布局

---
+++ src/views/main/system/user/c-cpns/user-search.vue
@@ -0,0 +1,52 @@
+<script setup lang='ts'>
+
+</script>
+
+<template>
+  <div class="user-search">
+    <el-form label-width="160px">
+      <el-row>
+        <el-col :span="8">
+          <el-form-item label="用户名">
+            <el-input placeholder="请输入查询的用户名"></el-input>
+          </el-form-item></el-col>
+        <el-col :span="8">
+          <el-form-item label="真实姓名">
+            <el-input placeholder="请输入查询的真实姓名"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="8">
+          <el-form-item label="手机号码">
+            <el-input placeholder="请输入查询的手机号码"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="8">
+          <el-form-item label="状态">
+            <el-select placeholder="请选择查询的状态">
+              <el-option label="启用" :value="1" />
+              <el-option label="禁用" :value="0" />
+            </el-select>
+          </el-form-item>
+        </el-col>
+        <el-col :span="8">
+          <el-form-item label="创建时间">
+            <el-date-picker type="daterange" start-placeholder="开始时间" end-placeholder="结束时间" />
+          </el-form-item>
+        </el-col>
+      </el-row>
+
+    </el-form>
+  </div>
+</template>
+
+<style lang='less' scoped>
+.user-search {
+  background-color: #fff;
+  padding: 20px;
+  margin-bottom: 0;
+}
+
+.el-form-item {
+  padding: 5px 30px;
+}
+</style>
--- src/views/main/system/user/user.vue
+++ src/views/main/system/user/user.vue
@@ -1,10 +1,17 @@
 <script setup lang='ts'>
-
+import UserSearch from '@/views/main/system/user/c-cpns/user-search.vue'
 </script>
 
 <template>
   <div class="user">
-    <h2>user</h2>
+    <div class="search">
+      <user-search />
+    </div>
+    <div class="content">
+      <div class="header">头部</div>
+      <div class="table">表格</div>
+      <div class="pagenation">分页</div>
+    </div>
   </div>
 </template>
 
