78b9590c78f172a3ce780c49844bb03081373fd6
Milk-Ice <mathila1218@gmail.com>
2023-10-25 11:05:55 +0800

    style(user): 05_用户User界面_自定义Table的Column

--- src/views/main/system/user/c-cpns/user-content.vue
+++ src/views/main/system/user/c-cpns/user-content.vue
@@ -12,11 +12,22 @@ const { usersList } = storeToRefs(systemStore)
 
 <template>
   <div class="user-content">
-    <div class="header">头部</div>
     <div class="table">
-      <ul>
-        <li v-for="item in usersList" :key="item.id">{{ item.name }}</li>
-      </ul>
+      <el-table :data="usersList" style="width: 100%" border>
+        <el-table-column type="selection" width="55" />
+        <el-table-column label="序号" type="index" width="55" />
+
+        <el-table-column prop="name" label="用户名" width="180" align="center" />
+        <el-table-column prop="realname" label="真实姓名" width="180" align="center" />
+        <el-table-column prop="cellphone" label="手机号码" width="180" align="center" />
+        <el-table-column prop="enable" label="状态" width="100" align="center" />
+        <el-table-column prop="createAt" label="创建时间" align="center" />
+        <el-table-column prop="updateAt" label="更新事件" align="center" />
+        <el-table-column label="操作" width="260" align="center">
+          <el-button type="primary" text icon="Edit">编辑</el-button>
+          <el-button type="danger" text icon="Delete">删除</el-button>
+        </el-table-column>
+      </el-table>
     </div>
     <div class="pagenation">分页</div>
   </div>
