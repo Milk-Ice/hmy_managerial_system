25846df5f34c2f525876be07dfa3f8ee80bb49ad
Milk-Ice <mathila1218@gmail.com>
2023-10-30 16:58:35 +0800

    feat(menu-modal): 08_编辑角色数据回显
    mapMenuListToIds、elTree.value?.setCheckKeys([])

--- src/hooks/usePageModal.ts
+++ src/hooks/usePageModal.ts
@@ -1,7 +1,10 @@
 import { ref } from "vue"
 import PageModal from '@/components/page-modal/page-modal.vue'
 
-function usePageModal() {
+// 回调函数的类型
+type EditFnType = (data: any) => void
+
+function usePageModal(editCallBack?: EditFnType) {
   const modalRef = ref<InstanceType<typeof PageModal>>()
 
   // 新增
@@ -10,8 +13,11 @@ function usePageModal() {
 
   }
   // 编辑
-  function HandleEditClick(formData: any) {
-    modalRef.value?.setModalVisible(false, formData)
+  function HandleEditClick(itemData: any) {
+    modalRef.value?.setModalVisible(false, itemData)
+    // 如果传过来了回调函数，就调用，并且把itemData传过去给回调函数
+    if (editCallBack) editCallBack(itemData)
+    // console.log(itemData)
   }
   return {
     modalRef,
@@ -19,4 +25,37 @@ function usePageModal() {
     HandleEditClick
   }
 }
+/**
+ * 将嵌套的菜单列表映射为包含所有菜单项的 ID 数组。
+ *
+ * @param {any[]} menuList - 嵌套的菜单列表
+ * @returns {number[]} - 包含所有菜单项的 ID 数组
+ */
+export function mapMenuListToIds(menuList: any[]) {
+  const ids: number[] = []
+
+  /**
+   * 递归遍历菜单列表并将每个叶子菜单项的 ID 添加到 ids 数组中。
+   *
+   * @param {any[]} menu - 当前的菜单列表
+   */
+  function recurseGetId(menu: any[]) {
+    for (const item of menu) {
+      if (item.children) {
+        // 继续递归处理子菜单
+        recurseGetId(item.children)
+      } else {
+        // 添加叶子菜单项的 ID 到数组
+        ids.push(item.id)
+      }
+    }
+  }
+
+  // 调用递归函数以开始遍历初始菜单列表
+  recurseGetId(menuList)
+
+  // 返回包含所有菜单项的 ID 数组
+  return ids
+}
+
 export default usePageModal
--- src/views/main/system/role/role.vue
+++ src/views/main/system/role/role.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, nextTick } from 'vue'
 import PageSearch from '@/components/page-search/page-search.vue'
 import PageContent from '@/components/page-content/page-content.vue'
 import PageModal from '@/components/page-modal/page-modal.vue'
@@ -9,13 +9,15 @@ import contentConfig from '../role/config/content.config'
 import modalConfig from '../role/config/modal.config'
 import usePageContent from '@/hooks/usePageContent'
 import usePageModal from '@/hooks/usePageModal'
+import { mapMenuListToIds } from '@/hooks/usePageModal'
 import useMainStore from '@/store/main/main'
 import { storeToRefs } from 'pinia'
+import type { ElTree } from 'element-plus/lib/components/index.js'
 
 // 点击search、content的逻辑
 const { contentRef, HandleResetClick, HandleQueryClick } = usePageContent()
 // 点击content、modal的逻辑
-const { modalRef, HandleNewClick, HandleEditClick } = usePageModal()
+const { modalRef, HandleNewClick, HandleEditClick } = usePageModal(editCallBack)
 
 // 获取菜单
 const mainStore = useMainStore()
@@ -31,6 +33,16 @@ function handleCheckClick(data1: any, data2: any) {
   otherInfo.value = { menulist }
   // console.log(otherInfo)
 }
+const treeRef = ref<InstanceType<typeof ElTree>>()
+// 定义一个回调函数，传给usePageModal（Hook函数）
+function editCallBack(itemData: any) {
+  // console.log('点击了编辑', itemData.menuList)
+  // console.log(menuIds)
+  nextTick(() => {
+    const menuIds = mapMenuListToIds(itemData.menuList)
+    treeRef.value?.setCheckedKeys(menuIds)
+  })
+}
 </script>
 
 <template>
@@ -42,7 +54,7 @@ function handleCheckClick(data1: any, data2: any) {
     <page-modal :modal-config="modalConfig" :other-info="otherInfo" ref="modalRef">
       <!-- 具名插槽，命名为 "menulist" -->
       <template #menulist>
-        <el-tree :data="entireMenus" show-checkbox node-key="id" ref="tree"
+        <el-tree ref="treeRef" :data="entireMenus" show-checkbox node-key="id"
           :props="{ children: 'children', label: 'name' }" @check="handleCheckClick">
         </el-tree></template>
     </page-modal>
