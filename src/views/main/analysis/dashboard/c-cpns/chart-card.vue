5174b417db90d8da511bf5676c6b1c2a928a2c96
Milk-Ice <mathila1218@gmail.com>
2023-10-31 21:19:58 +0800

    style(chart-card dashboard): 10_数据统计-图表card组件的封装过程

---
+++ src/views/main/analysis/dashboard/c-cpns/chart-card.vue
@@ -0,0 +1,18 @@
+<script setup lang='ts'>
+interface IProps {
+  header?: string
+}
+withDefaults(defineProps<IProps>(), {
+  header: '卡片的头部标题'
+})
+</script>
+
+<template>
+  <div class="chart-card">
+    <el-card class="box-card" :header="header">
+      <slot></slot>
+    </el-card>
+  </div>
+</template>
+
+<style lang='less' scoped></style>
--- src/views/main/analysis/dashboard/dashboard.vue
+++ src/views/main/analysis/dashboard/dashboard.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { storeToRefs } from 'pinia';
 import countCard from './c-cpns/count-card.vue'
+import chartCard from './c-cpns/chart-card.vue'
 import useAnalysisStore from '@/store/main/analysis/analysis'
 // 1.发起数据的亲求
 const analysisStore = useAnalysisStore()
@@ -11,6 +12,7 @@ const { amountList } = storeToRefs(analysisStore)
 
 <template>
   <div class="dashboard">
+    <!-- 数据统计 -->
     <el-row :gutter="10">
       <template v-for="item in amountList" :key="item.amount">
         <el-col :span="6">
@@ -18,12 +20,35 @@ const { amountList } = storeToRefs(analysisStore)
         </el-col>
       </template>
     </el-row>
+    <!-- 中间的图表 -->
+    <el-row :gutter="20">
+      <el-col :span="7">
+        <chart-card> 饼图</chart-card>
+      </el-col>
+      <el-col :span="10">
+        <chart-card> 饼图2</chart-card>
+      </el-col>
+      <el-col :span="7">
+        <chart-card> 饼图3</chart-card>
+      </el-col>
+    </el-row>
+
+    <!-- 底部的图表 -->
+    <el-row :gutter="20">
+      <el-col :span="12">
+        <chart-card> 折线图1</chart-card>
+      </el-col>
+      <el-col :span="12">
+        <chart-card> 折线图2</chart-card>
+      </el-col>
+
+    </el-row>
   </div>
 </template>
 
 
 <style lang="less" scoped>
-.dashboard {
-  color: red;
+.el-row {
+  margin-top: 10px;
 }
 </style>
