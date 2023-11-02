c26da5580d62f939a6adfd1e0cfa5e9af5c8974b
Milk-Ice <mathila1218@gmail.com>
2023-11-02 23:43:31 +0800

    style(readme): 完成readme文档的书写

--- src/views/main/analysis/overview/overview.vue
+++ src/views/main/analysis/overview/overview.vue
@@ -1,13 +1,32 @@
 <template>
-  <div class="overview">
-    <h2>overview</h2>
-  </div>
+  <el-card class="box-card">
+    <template #header>
+      <div class="card-header">
+        <span>核心技术</span>
+        <el-button class="button" text
+          >只有了解真相才能获取真正的自由~</el-button
+        >
+      </div>
+    </template>
+    <div class="content">
+      <div class="header">
+        本项目是基于Vue3 + TS + Vite + Pinia + Echarts,实现的一个后台管理系统
+      </div>
+      <div class="intro">
+        大致的功能点有：权限控制，登录不同账号系统会显示不同的菜单栏、常见的增删改查、Element-plus的使用、Echarts5.x的基本使用、富文本编辑器的使用等
+      </div>
+
+      <div class="footer">在该项目中将主要采用组件化、模块化的思想</div>
+    </div>
+  </el-card>
 </template>
 
 <script setup lang="ts"></script>
 
 <style lang="less" scoped>
-.overview {
-  color: red;
+.card-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 </style>
